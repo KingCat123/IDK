92983d9a793bd70df2858976bd4bd4a34587a37b
levy <100963276+4levy@users.noreply.github.com>
2024-02-27 09:32:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const largeImages = [
     'https://media.discordapp.net/attachments/1199386824582373516/1211343738656399392/hikikomari-terakomari.gif?ex=65eddac4&is=65db65c4&hm=54a4cb5a3bd8a1cdba46916a0f0c44251bdc685173a7cd90b338f6a444531bb6&=',
     'https://media.discordapp.net/attachments/1199386824582373516/1211343739323158638/hikikomari-hikikomari-kyuuketsuki.gif?ex=65eddac5&is=65db65c5&hm=af0f79d45c17475abf206a9b2c947239617364f2557abbf6a3433ee55714da8e&=',
     'https://cdn.discordapp.com/attachments/1199386824582373516/1211690815592726638/F4tviVKWYAAWVDT.jpg?ex=65ef1e02&is=65dca902&hm=11810f6fcd089278f9e12ad59f27a1b87dabda5021029933e7f25dcb6e974f84&',
+
+'https://cdn.discordapp.com/attachments/889976848581287946/1192664457915146301/download_1.gif?ex=65ea7fd5&is=65d80ad5&hm=af013b2b65d05bfd5ab5fef6afdce92752b4fee21ab9f20ba2ea3bc66d6a1faa&',
+
+'https://cdn.discordapp.com/attachments/889976848581287946/1193140899987865600/Herrscher.of.the.Void.full.2866346.gif?ex=65ec3b8e&is=65d9c68e&hm=28866edb01c0c0e44950eb9f45856c54cf986c0bccb7b3d680a496d50e585718&',
       // ใส่เพิ่มได้ถ้าเองต้องการ รูปใหญ่
   ];
 
@@ -40,7 +44,7 @@ const r = new Discord.RichPresence()
           .setStartTimestamp(startedAt)
           .setAssetsLargeText(`〈${currentDate}〉|〈🛸 ${Math.round(client.ws.ping)} m/s〉`) // status
           .setAssetsLargeImage(largeImages[currentLargeImageIndex]) // รูปใหญ่ไปใส่ข้างบน
-          .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1199386824582373516/1211367040057745478/395796082_1424688225118968_1183873637443851474_n.jpg?ex=65edf078&is=65db7b78&hm=406e4fe31e214013c1c675e06f3a2d8727773c08a1b21de18ae926f8a0aaa935&') // รูปเล็ก
+          .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1061395766586921092/1211763226577207416/Art_Icon.jpg?ex=65ef6172&is=65dcec72&hm=16829d77eeee2784c3c834ecfe0084e0d0909cea2b6a99d881c43683d451d392&') // รูปเล็ก
           .setAssetsSmallText('✧ Busy')
           .addButton('..?❤️', 'https://youtu.be/DG2QqcHwNdE?si=gmhUjuYRhkdObNhz')
           .addButton('Itz4levy', 'https://guns.lol/4levy')
